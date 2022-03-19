b7f4fb6e6ab03b34887acdcdf2adfa0b8d3fb90d
Christian Mendoza <patts2810@gmail.com>
2022-03-19 09:16:52 -0500

    feat: add styles to login

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -1,16 +1,18 @@
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { LoginScreen } from '../Components/auth/LoginScreen'
-import { RegisterScreen } from '../Components/auth/RegisterScreen'
+import { Switch, Route, Redirect } from "react-router-dom";
+import { LoginScreen } from "../Components/auth/LoginScreen";
+import { RegisterScreen } from "../Components/auth/RegisterScreen";
 
 export const AuthRouter = () => {
   return (
-    <div>
-      <Switch>
-          <Route exact path={"/auth/login"} component={ LoginScreen } />
-          <Route exact path={"/auth/register"} component={ RegisterScreen } />
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route exact path={"/auth/login"} component={LoginScreen} />
+          <Route exact path={"/auth/register"} component={RegisterScreen} />
 
-          <Redirect to={ '/auth/login' } />
+          <Redirect to={"/auth/login"} />
         </Switch>
+      </div>
     </div>
-  )
-}
+  );
+};
